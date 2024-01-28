783af28a855c0f214c31a5c86e388eddbb8ff810
federico <carlos.federico005@gmail.com>
2024-01-27 21:28:06 -0300

    feat: Add map page to properties

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,121 +1,190 @@
-import Sidebar from './components/sidebar/Sidebar';
-import Topbar from './components/topbar/Topbar';
-import Logo from './components/logo/Logo';
-import { createTheme, Box, ThemeProvider, Grid } from '@mui/material';
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import BusinessIcon from "@mui/icons-material/Business";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
-import LoginIcon from "@mui/icons-material/Login";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CommonLayout from './layouts/root/RootLayout';
 import DashboardPage from './pages/dashboard/DashboardPage';
 import ProfilePage from './pages/profile/ProfilePage';
 import CompaniesPage from './pages/companies/CompaniesPage';
-import UsersPage from './pages/users/UsersPage';
-import ReportsPage from './pages/reports/ReportsPage';
+import PropertiesPage from './pages/properties/PropertiesListPage';
+import PropertyDetailsPage from './pages/properties/PropertiesDetailsPage';
+import PropertiesMapPage from './pages/properties/PropertiesMapPage';
+import NewPropertyPage from './pages/properties/create/PropertiesCreatePage';
+import LotsPage from './pages/lots/LotsListPage';
+import NewLotPage from './pages/lots/LotsCreatePage';
+import LotDetailsPage from './pages/lots/LotsDetailsPage';
+import UsersPage from './pages/users/UsersListPage';
+import UserDetailsPage from './pages/users/details/UsersDetailsPage';
 import LoginPage from './pages/login/LoginPage';
-import './App.css'
+import InferenceFormPage from './pages/inference/InferenceCreatePage';
+import NotFoundPage from './pages/notfound/NotFound';
+import InferencesListPage from './pages/inference/InferencesListPage';
+import InferenceDetailsPage from './pages/inference/InferenceDetailsPage';
+import UserNewPage from './pages/users/UsersNewPage';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import AuthContext from './contexts/AuthProvider';
+import { Api } from './api/client';
+import './i18n/i18n';
 
-const sidebarData = [{
-  title: "Dashboard",
-  Icon: DashboardIcon,
-  to: "/dashboard",
-}, {
-  title: "Profile",
-  Icon: AccountBoxIcon,
-  to: "/profile",
-}, {
-  title: "Companies",
-  Icon: BusinessIcon,
-  to: "/companies",
-}, {
-  title: "Users",
-  Icon: PeopleIcon,
-  to: "/users",
-}, {
-  title: "Reports",
-  Icon: BarChartIcon,
-  to: "/reports",
-}, {
-  title: "Settings",
-  Icon: SettingsApplicationsIcon,
-  to: "/settings",
-}, {
-  title: "Login",
-  Icon: LoginIcon,
-  to: "/login",
-}, {
-  title: "Logout",
-  Icon: LogoutIcon,
-  to: "/logout",
-}];
 
-const CommonLayout = ({sidebarData}) => {
-  return (
-    <Grid container spacing={2} sx={{
-      padding: 3,
-    }}>
-      <Grid item xs={2}>
-        <Logo />
-      </Grid>
-      <Grid item xs={10}>
-        <Topbar />
-      </Grid>
-      <Grid item xs={2}>
-        <Sidebar data={sidebarData} />
-      </Grid>
-      <Grid item xs={10}>
-        <Outlet/>
-      </Grid>
-    </Grid>
-  );
+const queryClient = new QueryClient();
+
+const Logout = () => {
+  const { onLogout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onLogout();
+    navigate("/login");
+  });
+
+  return <span>Loading...</span>;
 };
 
+const RootComponent = () => {
+  const { auth } = useContext(AuthContext);
+  if(!auth.isAuthenticated) return <Navigate to="/login" />;
+  return <CommonLayout />;
+};
+
+const LoginComponent = () => {
+  const { auth } = useContext(AuthContext);
+  if(auth.isAuthenticated) return <Navigate to="/" />;
+  return <LoginPage />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootComponent />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      }, {
+        path: "profile",
+        element: <ProfilePage />,
+      }, {
+        path: "companies",
+        element: <CompaniesPage />,
+      }, {
+        path: "properties",
+        children: [
+          {
+            path: "",
+            element: <PropertiesPage />,
+          }, {
+            path: ":id",
+            children: [
+              {
+                path: "",
+                element: <PropertyDetailsPage />,
+              },
+              {
+                path: "map",
+                element: <PropertiesMapPage />
+              }
+            ]
+          }, {
+            path: "new",
+            element: <NewPropertyPage />,
+          }
+        ]
+      }, {
+        path: "lots",
+        children: [
+          {
+            path: "",
+            element: <LotsPage />,
+          },
+          {
+            path: ":id",
+            element: <LotDetailsPage />
+          },
+          {
+            path: "new",
+            element: <NewLotPage />
+          }
+        ]
+      }, {
+        path: "users",
+        children: [
+          {
+            path: "",
+            element: <UsersPage />,
+          },
+          {
+            path: ":id",
+            element: <UserDetailsPage />,
+          },
+          {
+            path: "new",
+            element: <UserNewPage />,
+          }
+        ],
+      },{
+        path: "inference",
+        children: [
+          {
+            path: "",
+            element: <InferencesListPage />,
+          },
+          {
+            path: ":id",
+            element: <InferenceDetailsPage />,
+          },
+          {
+            path: "new",
+            element: <InferenceFormPage />,
+          }
+        ],
+      }
+    ],
+  }, {
+    path: "/login",
+    element: <LoginComponent />,
+  }, {
+    path: "/logout",
+    element: <Logout />,
+  }
+]);
+
 function App() {
+  const { auth, setAuth } = useContext(AuthContext);
   const theme = createTheme();
 
-  return (<ThemeProvider theme={theme}>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="*" element={<CommonLayout sidebarData={sidebarData}/>}>
-        <Route index element={<DashboardPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="companies" element={<CompaniesPage />} />
-        <Route path="users" element={<UsersPage />} />
-        <Route path="reports" element={<ReportsPage />} />
-        <Route path="settings" element={<h1>Settings</h1>} />
-        <Route path="logout" element={<h1>Logout</h1>} />
-      </Route>
-    </Routes>
+  useEffect(() => {
+    const retrieveUserDataFromServer = async () => {
+      const data = (await Api.whoami()).data;
+      setAuth({
+        ...auth,
+        id: data.id,
+        email: data.email,
+        firstname: data.first_name,
+        lastname: data.last_name,
+        role: data.is_company_manager? 'Manager' : 'User',
+        company: {
+          id: data.company.id,
+          name: data.company.name,
+        }
+      });
+    };
 
-  </ThemeProvider>
+    if(auth.isAuthenticated) retrieveUserDataFromServer();
+  }, [auth.isAuthenticated]);
+
+
+
+  return (
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
   )
 }
 
 export default App;
-
-/*<Grid container spacing={2} sx={{
-        padding: 3,
-      }}>
-        <Grid item xs={2}>
-          <Logo />
-        </Grid>
-        <Grid item xs={10}>
-          <Topbar />
-        </Grid>
-        <Grid item xs={2}>
-          <Sidebar data={sidebarData} />
-        </Grid>
-        <Grid item xs={10}>
-          <Routes>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/companies" element={<CompaniesPage />} />
-            <Route path="/users" element={<UsersPage />} />
-            <Route path="/reports" element={<ReportsPage />} />
-          </Routes>
-        </Grid>
-      </Grid>*/
