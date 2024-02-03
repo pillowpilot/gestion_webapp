aa5de6bebb0b15c8508eee2740e84814ad7ea83f
federico <carlos.federico005@gmail.com>
2024-02-03 16:21:12 -0300

    fix: place page header into form. refactor: move layout components out of form components.

--- frontend/src/pages/properties/PropertiesDetailsPage.jsx
+++ frontend/src/pages/properties/PropertiesDetailsPage.jsx
@@ -17,7 +17,7 @@ import AuthContext from "../../contexts/AuthProvider";
 import { useTranslation } from "react-i18next";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
-const FeedbackAlert = ({ mutation }) => {
+const FeedbackAlert = ({ mutation, errors }) => {
   const { t } = useTranslation();
 
   if (mutation.isSuccess)
@@ -28,6 +28,7 @@ const FeedbackAlert = ({ mutation }) => {
     );
 
   if (mutation.isError) {
+    console.log("feedback mutation", mutation);
     const errorCode = mutation?.error?.code;
     if (errorCode === "ERR_NETWORK")
       return <Alert severity="error">{t("errors.network.default")}</Alert>;
@@ -39,6 +40,8 @@ const FeedbackAlert = ({ mutation }) => {
       </Alert>
     );
   }
+
+  return <></>;
 };
 
 const LoadingPropertyForm = () => {
@@ -46,32 +49,30 @@ const LoadingPropertyForm = () => {
   const navigate = useNavigate();
 
   return (
-    <Paper sx={{ p: 5 }}>
-      <Stack spacing={5}>
-        <Skeleton variant="rounded" width={420} height={60} />
-        <Stack direction="row" justifyContent="center" gap={1}>
-          <Button variant="outlined" size="medium" onClick={() => navigate(-1)}>
-            {t("properties.details.goBackBtn")}
-          </Button>
-          <Button disabled type="submit" variant="contained">
-            {t("properties.details.saveBtn")}
-          </Button>
-        </Stack>
+    <Stack spacing={5}>
+      <Skeleton variant="rounded" width={420} height={60} />
+      <Stack direction="row" justifyContent="center" gap={1}>
+        <Button variant="outlined" size="medium" onClick={() => navigate(-1)}>
+          {t("properties.details.goBackBtn")}
+        </Button>
+        <Button disabled type="submit" variant="contained">
+          {t("properties.details.saveBtn")}
+        </Button>
       </Stack>
-    </Paper>
+    </Stack>
   );
 };
 
-const PropertyForm = ({ data, mutation }) => {
+const PropertyForm = ({ formMethods, data, mutation }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { auth } = useContext(AuthContext);
 
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      name: data.data?.name,
-    },
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+  } = formMethods;
 
   const onSubmit = (data) => {
     mutation.mutate({
@@ -86,38 +87,33 @@ See: https://stackoverflow.com/a/76688881
 */
 
   return (
-    <Paper sx={{ p: 5 }}>
-      <Stack>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          style={{
-            width: "100%",
-          }}
-        >
-          <Stack spacing={5}>
-            <TextField
-              {...register("name")}
-              required
-              label="Name"
-              InputLabelProps={{ shrink: true }}
-            />
-            <FeedbackAlert mutation={mutation} />
-            <Stack direction="row" justifyContent="center" gap={1}>
-              <Button
-                variant="outlined"
-                size="medium"
-                onClick={() => navigate(-1)}
-              >
-                {t("properties.details.goBackBtn")}
-              </Button>
-              <Button type="submit" variant="contained">
-                {t("properties.details.saveBtn")}
-              </Button>
-            </Stack>
-          </Stack>
-        </form>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        width: "100%",
+      }}
+    >
+      <Stack spacing={5}>
+        <TextField
+          {...register("name", {
+            required: t("properties.details.errors.requiredName"),
+          })}
+          label={t("properties.details.labels.name")}
+          InputLabelProps={{ shrink: true }}
+          error={errors.name}
+          helperText={errors.name?.message}
+        />
+        <FeedbackAlert mutation={mutation} errors={errors} />
+        <Stack direction="row" justifyContent="center" gap={1}>
+          <Button variant="outlined" size="medium" onClick={() => navigate(-1)}>
+            {t("properties.details.goBackBtn")}
+          </Button>
+          <Button type="submit" variant="contained">
+            {t("properties.details.saveBtn")}
+          </Button>
+        </Stack>
       </Stack>
-    </Paper>
+    </form>
   );
 };
 
@@ -125,24 +121,48 @@ const PropertyDetailsPage = () => {
   const { t } = useTranslation();
   const { id } = useParams();
 
+  const formMethods = useForm();
+  const { setError, setValue } = formMethods;
+
   const queryClient = useQueryClient();
-  const retrieveProperty = useQuery(["properties", id], () =>
-    Api.retrieveProperty(id)
-  );
+  const retrieveProperty = useQuery({
+    queryKey: ["properties", id],
+    queryFn: () => Api.retrieveProperty(id),
+    onSuccess: (data) => {
+      setValue("name", data.data.name);
+    },
+  });
   const mutation = useMutation({
     mutationFn: (data) => Api.updateProperty(id, data),
     onSuccess: () => {
       queryClient.invalidateQueries(["properties"]);
-    }
+    },
+    onError: (error) => {
+      console.log("errormut", error);
+      const errorsData = error?.response?.data;
+      if (errorsData?.detail)
+        setError("root.serverError", {
+          type: "400",
+          message: errorsData.detail,
+        });
+      if (errorsData?.name)
+        setError("name", { type: "400", message: errorsData.name });
+    },
   });
 
   if (retrieveProperty.isLoading)
     return (
       <Stack spacing={10}>
-        <Typography variant="h4">{t("properties.details.header")}</Typography>
         <Stack direction="row" justifyContent="center">
           <Box sx={{ width: 500 }}>
-            <LoadingPropertyForm />
+            <Paper sx={{ p: 5 }}>
+              <Stack gap={5}>
+                <Typography variant="h4">
+                  {t("properties.details.header")}
+                </Typography>
+                <LoadingPropertyForm />
+              </Stack>
+            </Paper>
           </Box>
         </Stack>
       </Stack>
@@ -150,10 +170,20 @@ const PropertyDetailsPage = () => {
 
   return (
     <Stack spacing={10}>
-      <Typography variant="h4">{t("properties.details.header")}</Typography>
       <Stack direction="row" justifyContent="center">
         <Box sx={{ width: 500 }}>
-          <PropertyForm data={retrieveProperty.data} mutation={mutation} />
+          <Paper sx={{ p: 5 }}>
+            <Stack gap={5}>
+              <Typography variant="h4">
+                {t("properties.details.header")}
+              </Typography>
+              <PropertyForm
+                formMethods={formMethods}
+                data={retrieveProperty.data}
+                mutation={mutation}
+              />
+            </Stack>
+          </Paper>
         </Box>
       </Stack>
     </Stack>
