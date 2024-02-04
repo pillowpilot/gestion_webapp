7126104e87d9039c3de6061ebad3706c501c80ce
federico <carlos.federico005@gmail.com>
2024-02-03 23:09:35 -0300

    feat: make properties map use all available space. refactor: extract page layout

--- frontend/src/pages/properties/PropertiesMapPage.jsx
+++ frontend/src/pages/properties/PropertiesMapPage.jsx
@@ -1,50 +1,54 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Stack, Paper, Typography, Button, Skeleton } from "@mui/material";
+import { Stack, Paper, Typography, Button, Skeleton } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import bbox from "@turf/bbox";
 import * as L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, GeoJSON } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-// import geodata from "../../assets/map.json";
 import { Api } from "../../api/client";
 
-const LoadingPropertiesMapPage = () => {
+const PageLayout = ({ children }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+
   return (
-    <Stack>
-      <Typography variant="h4">{t("properties.map.header")}</Typography>
-      <Stack direction="row" justifyContent="center">
-        <Box>
-          <Paper sx={{ p: 5 }}>
-            <Stack gap={1} alignItems="center">
-              <Skeleton variant="rectangular" width={500} height={500} />
-              <Button
-                variant="outlined"
-                size="medium"
-                onClick={() => navigate(-1)}
-              >
-                {t("properties.map.goBackBtn")}
-              </Button>
-            </Stack>
-          </Paper>
-        </Box>
+    <Paper sx={{ p: 5 }}>
+      <Stack
+        gap={1}
+        sx={{
+          width: "100%",
+          height: "calc(100vh - 210px)", // TODO Replace somehow magic number
+        }}
+      >
+        <Typography variant="h4">{t("lots.map.header")}</Typography>
+        {children}
+        <Stack direction="row" justifyContent="center">
+          <Button variant="outlined" size="medium" onClick={() => navigate(-1)}>
+            {t("lots.map.goBackBtn")}
+          </Button>
+        </Stack>
       </Stack>
-    </Stack>
+    </Paper>
+  );
+};
+
+const LoadingMapPage = () => {
+  return (
+    <PageLayout>
+      <Skeleton variant="rectangular" width="100%" height="100%" />
+    </PageLayout>
   );
 };
 
 const PropertiesMapPage = () => {
   const { id } = useParams();
-  const { t } = useTranslation();
-  const navigate = useNavigate();
 
   const retrieveProperty = useQuery(["properties", id], () =>
     Api.retrieveProperty(id)
@@ -68,50 +72,33 @@ const PropertiesMapPage = () => {
   }, []);
 
   if (retrieveProperty.isLoading || geodataQuery.isLoading)
-    return <LoadingPropertiesMapPage />;
-
-  console.log('property', retrieveProperty);
-  console.log('geodata', geodataQuery);
+    return <LoadingMapPage />;
   
   const [minX, minY, maxX, maxY] = bbox(geodataQuery.data);
   const mapBounds = new L.LatLngBounds([
     [minY, minX],
     [maxY, maxX],
   ]);
-  console.log(mapBounds);
 
   return (
-    <Stack spacing={10}>
-      <Typography variant="h4">{t("properties.map.header")}</Typography>
-      <Stack direction="row" justifyContent="center">
-        <Box>
-          <Paper sx={{ p: 5 }}>
-            <Stack gap={1} alignItems="center">
-              <MapContainer
-                bounds={mapBounds}
-                style={{ width: "1200px", height: "500px" }}
-              >
-                <GeoJSON key="cuack" data={geodataQuery.data}>
-                  <Popup>Hi!</Popup>
-                </GeoJSON>
+    <PageLayout>
+      <MapContainer
+        bounds={mapBounds}
+        style={{
+          flexGrow: 1,
+          width: "100%",
+        }}
+      >
+        <GeoJSON key="cuack" data={geodataQuery.data}>
+          <Popup>Hi!</Popup>
+        </GeoJSON>
 
-                <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-              </MapContainer>
-              <Button
-                variant="outlined"
-                size="medium"
-                onClick={() => navigate(-1)}
-              >
-                {t("properties.map.goBackBtn")}
-              </Button>
-            </Stack>
-          </Paper>
-        </Box>
-      </Stack>
-    </Stack>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </PageLayout>
   );
 };
 
