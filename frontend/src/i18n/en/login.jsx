d7e34110426ab095f581af62bc05af79dd889ef5
federico <carlos.federico005@gmail.com>
2024-02-03 22:10:00 -0300

    fix: add login labels into en/login translation file

--- frontend/src/i18n/en/login.jsx
+++ frontend/src/i18n/en/login.jsx
@@ -3,6 +3,10 @@ const t = {
     caption: "Welcome back!",
     loginBtn: "Log in",
     dontHaveAccount: "Don't have an account?",
+    labels: {
+        email: "Email",
+        password: "Password",
+    }
 };
 
 export default t;
