7f5e1861527f91fd6b9a1aa6e776cc613937b64c
federico <carlos.federico005@gmail.com>
2024-02-05 02:18:36 -0300

    refactor: split PropertiesCreatePage into its components. manage errors with snackbars. use react-query for mutation.

--- frontend/src/pages/properties/create/Form.jsx
+++ frontend/src/pages/properties/create/Form.jsx
@@ -1,43 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Button,
-  Alert,
-  Stack,
-  TextField,
-} from "@mui/material";
+import { Button, Alert, Stack, TextField } from "@mui/material";
 import { Controller } from "react-hook-form";
-import { MuiFileInput } from 'mui-file-input';
+import { MuiFileInput } from "mui-file-input";
 import { useTranslation, withTranslation } from "react-i18next";
+import AuthContext from "../../../contexts/AuthProvider";
 
+const Form = ({ t, formMethods, mutation }) => {
+  const { auth } = useContext(AuthContext);
 
-const FormErrorMessage = ({ flag, msg }) =>
-  flag ? <Alert severity="error">{msg}</Alert> : <></>;
-
-const SuccessfullSubmitMessage = ({ flag }) => {
-  const { t } = useTranslation();
-  if (!flag) return <></>;
-  return (
-    <Alert severity="success">{t("properties.create.createSuccessMsg")}</Alert>
-  );
-};
-
-const Form = ({ t, formMethods, onSubmitHandler }) => {
   const navigate = useNavigate();
 
   const {
     register,
     handleSubmit,
     control,
-    formState: { errors, isSubmitSuccessful },
+    formState: { errors },
   } = formMethods;
 
+  const onSubmit = (d) => {
+    mutation.mutate({
+      name: d.name,
+      company: auth.company.id,
+      geodata: d.geodata,
+    });
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmitHandler)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Stack
         spacing={3}
         sx={{
-          sm: {minWidth: "350px",}
+          sm: { minWidth: "350px" },
         }}
       >
         <TextField
@@ -48,28 +42,24 @@ const Form = ({ t, formMethods, onSubmitHandler }) => {
           error={errors.name}
           helperText={errors.name?.message}
         />
-        <Controller 
+        <Controller
           name="geodata"
           control={control}
           rules={{
             required: t("properties.create.errors.requiredGeodata"),
           }}
-          render={({field, fieldState}) => {
-            console.log("field", field);
-            console.log("fieldState", fieldState);
-            return <MuiFileInput {...field} 
-              label={t("properties.create.labels.geodata")}
-              inputProps={{ accept: ".geojson" }}
-              error={fieldState.invalid}
-              helperText={fieldState.invalid? fieldState.error?.message : ""}
-            />
+          render={({ field, fieldState }) => {
+            return (
+              <MuiFileInput
+                {...field}
+                label={t("properties.create.labels.geodata")}
+                inputProps={{ accept: ".geojson" }}
+                error={fieldState.invalid}
+                helperText={fieldState.error?.message}
+              />
+            );
           }}
         />
-        <FormErrorMessage
-          flag={errors.root?.serverError}
-          msg={errors.root?.serverError?.message}
-        />
-        <SuccessfullSubmitMessage flag={isSubmitSuccessful} />
         <Stack direction="row" justifyContent="center" gap={1}>
           <Button variant="outlined" size="medium" onClick={() => navigate(-1)}>
             {t("properties.create.goBackBtn")}
--- frontend/src/pages/properties/create/PropertiesCreatePage.jsx
+++ frontend/src/pages/properties/create/PropertiesCreatePage.jsx
@@ -1,49 +1,60 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Paper, Stack, Typography } from "@mui/material";
+import { enqueueSnackbar } from "notistack";
 import { useForm } from "react-hook-form";
+import { useQueryClient, useMutation } from "react-query";
 import { Api } from "../../../api/client";
-import { withTranslation } from "react-i18next";
-import AuthContext from "../../../contexts/AuthProvider";
+import { withTranslation, useTranslation } from "react-i18next";
 import Form from "./Form";
+import { queryKeys } from "../queries";
 
-const NewPropertyPage = ({ t }) => {
-  const { auth } = useContext(AuthContext);
-  const formMethods = useForm();
-
-  const { setError } = formMethods;
-  const onSubmitHandler = async (data) => {
-    const name = data.name;
-    try {
-      await Api.createProperty({
-        name: name,
-        company: auth.company.id,
-        geodata: data.geodata,
-      });
-    } catch (err) {
-      const errorsData = err.response.data;
-      if (errorsData.detail)
-        setError("root.serverError", {
-          type: "400",
-          message: errorsData.detail,
-        });
-      if (errorsData.name)
-        setError("name", { type: "400", message: errorsData.name });
-      if (errorsData.geodata)
-        setError("geodata", { type: "400", message: errorsData.geodata });
-    }
-  };
+const PageLayout = ({ children }) => {
+  const { t } = useTranslation();
 
   return (
-    <Stack spacing={10}>
-      <Stack direction="row" justifyContent="center">
-        <Paper sx={{ p: 5 }}>
-          <Stack gap={5}>
-            <Typography variant="h4">{t("properties.create.header")}</Typography>
-            <Form formMethods={formMethods} onSubmitHandler={onSubmitHandler} />
-          </Stack>
-        </Paper>
-      </Stack>
+    <Stack direction="row" justifyContent="center">
+      <Paper sx={{ p: 5 }}>
+        <Stack gap={5}>
+          <Typography variant="h4">{t("properties.create.header")}</Typography>
+          {children}
+        </Stack>
+      </Paper>
     </Stack>
+  );
+};
+
+const NewPropertyPage = ({ t }) => {
+  const formMethods = useForm();
+  const { setError } = formMethods;
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (data) => Api.createProperty(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(queryKeys.all);
+      enqueueSnackbar(t("properties.create.createSuccessMsg"), {
+        variant: "success",
+      });
+    },
+    onError: (error) => {
+      if (error.response) {
+        const data = error.response.data;
+        if (data.detail) enqueueSnackbar(data.detail, { variant: "error" });
+        if (data.name) setError("name", { type: "400", message: data.name });
+        if (data.geodata)
+          setError("geodata", { type: "400", message: data.geodata });
+      } else if (error.request) {
+        enqueueSnackbar(t("errors.network.default"), { variant: "error" });
+      } else {
+        enqueueSnackbar(t("errors.unknown.default"), { variant: "error" });
+      }
+    },
+  });
+
+  return (
+    <PageLayout>
+      <Form formMethods={formMethods} mutation={mutation} />
+    </PageLayout>
   );
 };
 
