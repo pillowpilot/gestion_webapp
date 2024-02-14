836f2cbfcc41d91d52dadbc030a322002a15bc61
federico <carlos.federico005@gmail.com>
2024-02-14 03:39:50 -0300

    fix: add overflow-x: hidden to body. feat: clear cache on logout. refactor: refactor pages import. move queryclientprovider and snackbarprovider into main.jsx from App.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,44 @@
-import { useContext, useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import CommonLayout from './layouts/root/RootLayout';
-import DashboardPage from './pages/dashboard/DashboardPage';
-import ProfilePage from './pages/profile/ProfilePage';
-import CompaniesPage from './pages/companies/CompaniesPage';
-import PropertiesPage from './pages/properties/PropertiesListPage';
-import PropertyDetailsPage from './pages/properties/PropertiesDetailsPage';
-import PropertiesMapPage from './pages/properties/PropertiesMapPage';
-import NewPropertyPage from './pages/properties/create/PropertiesCreatePage';
-import LotsPage from './pages/lots/LotsListPage';
-import NewLotPage from './pages/lots/create/LotsCreatePage';
-import LotDetailsPage from './pages/lots/LotsDetailsPage';
-import LotsMapPage from './pages/lots/LotsMapPage';
-import UsersPage from './pages/users/UsersListPage';
-import UserDetailsPage from './pages/users/details/UsersDetailsPage';
-import LoginPage from './pages/login/LoginPage';
-import InferenceFormPage from './pages/inference/InferenceCreatePage';
-import NotFoundPage from './pages/notfound/NotFound';
-import InferencesListPage from './pages/inference/list/InferencesListPage';
-import InferenceDetailsPage from './pages/inference/InferenceDetailsPage';
-import UserNewPage from './pages/users/UsersNewPage';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import AuthContext from './contexts/AuthProvider';
-import { Api } from './api/client';
-import './i18n/i18n';
+import { useContext, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useQueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
+import AuthContext from "./contexts/AuthProvider";
+import { Api } from "./api/client";
+import "./i18n/i18n";
+import "./App.css";
 
-const queryClient = new QueryClient();
+// Pages
+import CommonLayout from "./layouts/root/RootLayout";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import LoginPage from "./pages/login/LoginPage";
+import NotFoundPage from "./pages/notfound/NotFound";
+import { DetailLot, ListLots, LotMap, CreateLot } from "./pages/lots";
+import { ListUsers, CreateUser, DetailsUser } from "./pages/users";
+import {
+  ListProperties,
+  CreateProperty,
+  DetailProperty,
+  PropertyMap,
+} from "./pages/properties";
+import {
+  DetailInference,
+  CreateInference,
+  ListInferences,
+} from "./pages/inference";
 
 const Logout = () => {
+  const queryClient = useQueryClient();
   const { onLogout } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  queryClient.clear();
   useEffect(() => {
     onLogout();
     navigate("/login");
@@ -45,126 +49,133 @@ const Logout = () => {
 
 const RootComponent = () => {
   const { auth } = useContext(AuthContext);
-  if(!auth.isAuthenticated) return <Navigate to="/login" />;
+  if (!auth.isAuthenticated) return <Navigate to="/login" />;
   return <CommonLayout />;
 };
 
 const LoginComponent = () => {
   const { auth } = useContext(AuthContext);
-  if(auth.isAuthenticated) return <Navigate to="/" />;
+  if (auth.isAuthenticated) return <Navigate to="/" />;
   return <LoginPage />;
 };
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootComponent />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "dashboard",
-        element: <DashboardPage />,
-      }, {
-        path: "profile",
-        element: <ProfilePage />,
-      }, {
-        path: "companies",
-        element: <CompaniesPage />,
-      }, {
-        path: "properties",
-        children: [
-          {
-            path: "",
-            element: <PropertiesPage />,
-          }, {
-            path: ":id",
-            children: [
-              {
-                path: "",
-                element: <PropertyDetailsPage />,
-              },
-              {
-                path: "map",
-                element: <PropertiesMapPage />
-              }
-            ]
-          }, {
-            path: "new",
-            element: <NewPropertyPage />,
-          }
-        ]
-      }, {
-        path: "lots",
-        children: [
-          {
-            path: "",
-            element: <LotsPage />,
-          },
-          {
-            path: ":id",
-            children: [
-              {
-                path: "",
-                element: <LotDetailsPage />,
-              },
-              {
-                path: "map",
-                element: <LotsMapPage />
-              }
-            ]
-          },
-          {
-            path: "new",
-            element: <NewLotPage />
-          }
-        ]
-      }, {
-        path: "users",
-        children: [
-          {
-            path: "",
-            element: <UsersPage />,
-          },
-          {
-            path: ":id",
-            element: <UserDetailsPage />,
-          },
-          {
-            path: "new",
-            element: <UserNewPage />,
-          }
-        ],
-      },{
-        path: "inference",
-        children: [
-          {
-            path: "",
-            element: <InferencesListPage />,
-          },
-          {
-            path: ":id",
-            element: <InferenceDetailsPage />,
-          },
-          {
-            path: "new",
-            element: <InferenceFormPage />,
-          }
-        ],
-      }
-    ],
-  }, {
-    path: "/login",
-    element: <LoginComponent />,
-  }, {
-    path: "/logout",
-    element: <Logout />,
-  }
-]);
 
 function App() {
   const { auth, setAuth } = useContext(AuthContext);
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
+  console.log(auth);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootComponent />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          path: "dashboard",
+          element: <DashboardPage />,
+        },
+        {
+          path: "profile",
+          element: <DetailsUser id={auth.id} />,
+        },
+        {
+          path: "properties",
+          children: [
+            {
+              path: "",
+              element: <ListProperties />,
+            },
+            {
+              path: ":id",
+              children: [
+                {
+                  path: "",
+                  element: <DetailProperty />,
+                },
+                {
+                  path: "map",
+                  element: <PropertyMap />,
+                },
+              ],
+            },
+            {
+              path: "new",
+              element: <CreateProperty />,
+            },
+          ],
+        },
+        {
+          path: "lots",
+          children: [
+            {
+              path: "",
+              element: <ListLots />,
+            },
+            {
+              path: ":id",
+              children: [
+                {
+                  path: "",
+                  element: <DetailLot />,
+                },
+                {
+                  path: "map",
+                  element: <LotMap />,
+                },
+              ],
+            },
+            {
+              path: "new",
+              element: <CreateLot />,
+            },
+          ],
+        },
+        {
+          path: "users",
+          children: [
+            {
+              path: "",
+              element: <ListUsers />,
+            },
+            {
+              path: ":id",
+              element: <DetailsUser />,
+            },
+            {
+              path: "new",
+              element: <CreateUser />,
+            },
+          ],
+        },
+        {
+          path: "inference",
+          children: [
+            {
+              path: "",
+              element: <ListInferences />,
+            },
+            {
+              path: ":id",
+              element: <DetailInference />,
+            },
+            {
+              path: "new",
+              element: <CreateInference />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <LoginComponent />,
+    },
+    {
+      path: "/logout",
+      element: <Logout />,
+    },
+  ]);
 
   useEffect(() => {
     const retrieveUserDataFromServer = async () => {
@@ -175,27 +186,23 @@ function App() {
         email: data.email,
         firstname: data.first_name,
         lastname: data.last_name,
-        role: data.is_company_manager? 'Manager' : 'User',
+        role: data.is_company_manager ? "Manager" : "User",
         company: {
           id: data.company.id,
           name: data.company.name,
-        }
+        },
       });
     };
 
-    if(auth.isAuthenticated) retrieveUserDataFromServer();
+    if (auth.isAuthenticated) retrieveUserDataFromServer();
   }, [auth.isAuthenticated]);
 
-
-
   return (
-      <ThemeProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </ThemeProvider>
-  )
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { SnackbarProvider } from "notistack";
+import { AuthProvider } from "./contexts/AuthProvider";
+import App from "./App.jsx";
+import "./index.css";
 
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
